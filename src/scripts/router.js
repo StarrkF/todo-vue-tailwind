db2e505a77e250f22e363feb86390e67f2159e65
StarrkF <faymergen98@gmail.com>
2023-04-15 21:23:52 +0300

    gh pages router bug fixed

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory  } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory('/todo-vue-tailwind/'),
+    history: createWebHashHistory('/todo-vue-tailwind/'),
     routes: [
         {
             path: '/',
